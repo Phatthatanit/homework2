0f3bbedfbacf75bf1792eca31fcc39da1644676d
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-08-14 16:23:34 +0700

    Add project files.

---
+++ hw2/hw2.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	char name[1000];
+	long salary;
+	printf("name :  ");
+	scanf("%s", &name);
+	printf("salary : ");
+	scanf("%ld", &salary);
+	int money = salary * 12;
+	printf("%s have money per year : %ld", name,money);
+	if (money >= 10000) {
+		int tex = money / 100;
+		printf("\ntex : %ld", tex);
+		printf("\n%s have total money : %ld ", name,money - tex);
+	}
+	else if (money < 9999) {
+		printf("\ntex : 0");
+		printf("\n%s have total money : %ld ", name,money);
+	}
+}
